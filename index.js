f03db113655e866f4a5186b7c23367cedc95e841
Nkemsy <ajekwe.nkem@gmail.com>
2024-02-07 04:53:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function togglePassword() {
+    const passwordInput = document.getElementById("passwordInput"); 
+    passwordInput.type === "password" ? passwordInput.type = "text" : passwordInput.type = "password";        
+} 
+
+function toglePassword() {
+    const passwordInput = document.getElementById("psswordInput"); 
+    passwordInput.type === "password" ? passwordInput.type = "text" : passwordInput.type = "password";        
+} 
